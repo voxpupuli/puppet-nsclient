7dbcddb6f9afe504efe1e219926938d923693945
Henrik Feldt <henrik@haf.se>
2014-08-05 15:44:38 +0200

    https

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class nsclient::params {
   $allowed_hosts           = []
   $service_state           = 'running'
   $service_enable          = true
-  $package_source_location = 'http://files.nsclient.org/stable'
+  $package_source_location = 'https://files.nsclient.org/stable'
   $package_name            = 'NSClient++ (x64)'
   $package_source          = 'NSCP-0.4.1.101-x64.msi'
   $download_destination    = "c:\\temp"
