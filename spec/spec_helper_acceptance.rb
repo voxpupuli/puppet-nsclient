da6fd516e829d461694bbd12f27831f904506abe
Tim Meusel <tim@bastelfreak.de>
2016-10-10 19:11:00 +0200

    rubocop: fix Style/StringLiterals

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -27,7 +27,7 @@ Spec.configure do |c|
 
 
       if host['platform'] =~ %r{windows}
-        endpoint = "http://127.0.0.1:5985/wsman"
+        endpoint = 'http://127.0.0.1:5985/wsman'
         c.winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, user: 'vagrant', pass: 'vagrant', basic_auth_only: true)
         c.winrm.set_timeout 300
       end
@@ -39,9 +39,9 @@ Spec.configure do |c|
       copy_module_to(host, source: proj_root, module_name: name)
 
 
-      on host, puppet('module', 'install', forge_repo, "puppetlabs-stdlib"), { acceptable_exit_codes: [0, 1] }
+      on host, puppet('module', 'install', forge_repo, 'puppetlabs-stdlib'), { acceptable_exit_codes: [0, 1] }
 
-      on host, puppet('module', 'install', forge_repo, "opentable-download_file"), { acceptable_exit_codes: [0, 1] }
+      on host, puppet('module', 'install', forge_repo, 'opentable-download_file'), { acceptable_exit_codes: [0, 1] }
     end
   end
 end
