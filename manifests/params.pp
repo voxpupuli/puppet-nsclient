f718073acdfc572303e7afe69015532fc3699e8b
Robert Stuart <rstuart@indue.com.au>
2019-07-18 15:41:50 +1000

    update chocolatey version to 0.5.2.39

--- manifests/params.pp
+++ manifests/params.pp
@@ -21,5 +21,5 @@ class nsclient::params {
   $password                   = undef
   $chocolatey_provider        = false
   $chocolatey_package_name    = 'nscp'
-  $chocolatey_package_version = '0.5.0.62'
+  $chocolatey_package_version = '0.5.2.39'
 }
